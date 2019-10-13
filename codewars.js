5ca768640d2ecdc83b84ecc455ab6f4c67f64655
lihle <lihlemenz@gmail.com>
2019-10-13 06:08:06 +0200

    added styling

---
+++ codewars.js
@@ -0,0 +1,19 @@
+// const inputVal = document.querySelector("#input");
+// const searchButton = document.querySelector(".searchButton");
+// const username = document.querySelector(".username");
+// const userUrl = document.querySelector(".userUrl");
+
+// // const api_key = "gSYRvQHkdf8F8UwF7UZ_";
+
+// // const fetchProfile = async (user) => {
+// //     const call_api = await fetch(`https://www.codewars.com/api/v1/users/${user}?api_key=${api_key}`);
+
+// //     // converting data to readable format
+// //     const data = await call_api.json();
+// //     return { data };
+// // };
+
+// searchButton.addEventListener("click", () => {
+//     console.log("clicked");
+// })
+
